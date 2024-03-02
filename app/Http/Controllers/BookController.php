1e4e85dc6e20afa6f707800c5646a92effd5a49f
Jorge Muniz <jorgemp221@msn.com>
2024-03-02 11:12:28 -0600

    add rating component remove cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -27,7 +27,8 @@ class BookController extends Controller
         };
 
         $cacheKey = 'books' . $filter . ':' . $title;
-        $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
+        // $books = cache()->remember($cacheKey, 3600, fn () => $books->get());
+        $books = $books->get();
 
         return view('books.index', ['books' => $books]);
     }
